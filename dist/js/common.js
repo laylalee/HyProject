3413d60cab5e2476a1dcfbc7dc471834a9f5bec8
leejing <896325728@qq.com>
2017-03-21 22:26:36 +0800

    列表页

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,7 +3,29 @@
  */
 ;
 (function (window, $) {
-    var tool = {};
+    var tool = {},
+        $consult = $('.com-consult'),
+        $menuFold=$('.com-menu-fold'),
+        $menuFoldTag=$('.com-top-search .menu'),
+        $window = $(window);
+    tool.winScroll=function(){
+        if ($window.scrollTop() > 500) {
+            $consult.fadeIn();
+        }else{
+            $consult.hide();
+        }
+    };
+    $menuFoldTag.on('click', function () {
+        $menuFold.addClass('open');
+        return false;
+    });
+    $window.on({
+        'click': function () {
+            $menuFold.removeClass('open');
+            return false;
+        },
+        'scroll': $.throttle(500, tool.winScroll)
+    });
     window.tool = tool;
 
 })(window, jQuery);
