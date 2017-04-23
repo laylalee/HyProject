330d406568aee5a16d22df9c4b8f706fd569b14a
leejing <896325728@qq.com>
2017-04-23 18:07:49 +0800

    详情。服务导航跟随调整

--- dist/js/common.js
+++ dist/js/common.js
@@ -4,6 +4,7 @@
 ;
 (function (window, $) {
     var tool = {},
+        $body = $('html, body'),
         $consult = $('.com-consult'),
         $menuFold=$('.com-menu-fold'),
         $menuFoldTag=$('.com-top-search .menu'),
@@ -15,6 +16,26 @@
             $consult.hide();
         }
     };
+    tool.eleHandel=function($target) {
+        var len = $target.length,
+            eleArray = [];
+        $target.each(function(){
+            eleArray.push($(this).offset().top );
+        })
+        return eleArray;
+    }
+    tool.tabPos=function($tab){
+        $('a', $tab).each(function (index, element) {
+        var _this = $(element),
+            $target = $(_this.attr('rel'));
+        _this.on('click', function () {
+            var offerTop = $target.offset().top;
+            $body.animate({
+                scrollTop: offerTop
+            }, 500);
+        })
+    });
+    }
     $menuFoldTag.on('click', function () {
         $menuFold.addClass('open');
         return false;
